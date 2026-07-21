7a6cac57850078572ae67a108b64c27dd8fe1c17
Gimicard <flummi0406@gmail.com>
2026-07-21 21:36:34 +0200

    Initial commit

---
+++ src/main/java/com/gimicard/oresrenewed/OresRenewedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.gimicard.oresrenewed;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OresRenewedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
