7d363b207e2ea00bbf3370cb3c87a66f922582cb
Gimicard <flummi0406@gmail.com>
2026-07-22 22:21:53 +0200

    ores sound

--- src/main/java/com/gimicard/oresrenewed/block/ModBlocks.java
+++ src/main/java/com/gimicard/oresrenewed/block/ModBlocks.java
@@ -19,69 +19,69 @@ public class ModBlocks {
 
     public static final Block URAN_ORE_BLOCK = registerBlock("uran_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block TITAN_ORE_BLOCK = registerBlock("titan_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block RUBY_ORE_BLOCK = registerBlock("ruby_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block LEAD_ORE_BLOCK = registerBlock("lead_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block KILLIUM_ORE_BLOCK = registerBlock("killium_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block MAGNETITE_ORE_BLOCK = registerBlock("magnetite_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block VULKANIT_ORE_BLOCK = registerBlock("vulkanit_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     public static final Block SCHATTENKRISTALL_ORE_BLOCK = registerBlock("schattenkristall_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
 
 
     public static final Block D_URAN_ORE_BLOCK = registerBlock("d_uran_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_TITAN_ORE_BLOCK = registerBlock("d_titan_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_RUBY_ORE_BLOCK = registerBlock("d_ruby_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_LEAD_ORE_BLOCK = registerBlock("d_lead_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_KILLIUM_ORE_BLOCK = registerBlock("d_killium_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_MAGNETITE_ORE_BLOCK = registerBlock("d_magnetite_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_VULKANIT_ORE_BLOCK = registerBlock("d_vulkanit_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     public static final Block D_SCHATTENKRISTALL_ORE_BLOCK = registerBlock("d_schattenkristall_ore_block",
             properties -> new Block(properties.strength(4F)
-                    .requiresCorrectToolForDrops().sound(SoundType.ANVIL)));
+                    .requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
 
     private static Block registerBlock(String name, Function<BlockBehaviour.Properties, Block> funktion) {
