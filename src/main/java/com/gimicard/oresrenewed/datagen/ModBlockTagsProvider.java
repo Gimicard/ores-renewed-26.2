560a1b103c8566d41598b17134264b0d663fd11c
Gimicard <flummi0406@gmail.com>
2026-07-23 23:21:28 +0200

    item lang and pickace tags

--- src/main/java/com/gimicard/oresrenewed/datagen/ModBlockTagsProvider.java
+++ src/main/java/com/gimicard/oresrenewed/datagen/ModBlockTagsProvider.java
@@ -16,9 +16,23 @@ public class ModBlockTagsProvider extends FabricTagsProvider.BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider registries) {
         tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.getRK(ModBlocks.BLACKCRISTAL_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.KILLIUM_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.LEAD_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.MAGNETITE_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.RUBY_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.TITAN_ORE_BLOCK))
                 .add(ModBlocks.getRK(ModBlocks.URAN_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.VULKANIT_ORE_BLOCK))
+
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_BLACKCRISTAL_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_KILLIUM_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_LEAD_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_MAGNETITE_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_RUBY_ORE_BLOCK))
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_TITAN_ORE_BLOCK))
                 .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_URAN_ORE_BLOCK))
-                        .add(ModBlocks.getRK(ModBlocks.BLACKCRISTAL_ORE_BLOCK));
+                .add(ModBlocks.getRK(ModBlocks.DEEPSLATE_VULKANIT_ORE_BLOCK));
 
 
         tag(BlockTags.NEEDS_DIAMOND_TOOL)
