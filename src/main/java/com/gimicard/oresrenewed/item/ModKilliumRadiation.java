8c93774b6b2212b170fba8108a1036b4b478cb9e
Gimicard <flummi0406@gmail.com>
2026-07-26 22:00:37 +0200

    killium items kill

--- src/main/java/com/gimicard/oresrenewed/item/ModKilliumRadiation.java
+++ src/main/java/com/gimicard/oresrenewed/item/ModKilliumRadiation.java
@@ -1,12 +1,15 @@
 package com.gimicard.oresrenewed.item;
 
+import com.gimicard.oresrenewed.block.ModBlocks;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.Item;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public class ModKilliumRadiation {
@@ -15,6 +18,14 @@ public class ModKilliumRadiation {
 
     private static final int DEATH_TICKS = 20 * DEATH_SECONDS;
 
+    private static final Set<Item> RADIOACTIVE_ITEMS = Set.of(
+            ModItems.RAW_KILLIUM,
+            ModItems.KILLIUM_INGOT,
+            ModBlocks.KILLIUM_ORE_BLOCK.asItem(),
+            ModBlocks.DEEPSLATE_KILLIUM_ORE_BLOCK.asItem(),
+            ModBlocks.BLOCK_OF_KILLIUM.asItem()
+    );
+
     private static final Map<UUID, Integer> exposureTicks = new HashMap<>();
 
     public static void register() {
@@ -22,7 +33,7 @@ public class ModKilliumRadiation {
             for (ServerPlayer player : server.getPlayerList().getPlayers()) {
                 UUID id = player.getUUID();
 
-                if (hasRawKillium(player.getInventory())) {
+                if (hasRadioactiveItem(player.getInventory())) {
                     int ticks = exposureTicks.merge(id, 1, Integer::sum);
 
                     if (ticks >= DEATH_TICKS) {
@@ -37,13 +48,12 @@ public class ModKilliumRadiation {
         });
     }
 
-    private static boolean hasRawKillium(Inventory inventory) {
+    private static boolean hasRadioactiveItem(Inventory inventory) {
         for (int i = 0; i < inventory.getContainerSize(); i++) {
-            if (inventory.getItem(i).is(ModItems.RAW_KILLIUM)) {
+            if (RADIOACTIVE_ITEMS.contains(inventory.getItem(i).getItem())) {
                 return true;
             }
         }
         return false;
     }
-
 }
